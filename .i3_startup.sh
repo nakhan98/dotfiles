13a601fc56f4b78d26c30f4c8425c460645edab8
Nasef Khan (crouton) <nakhan98@gmail.com>
2018-04-03 09:49:42 +0100

    Add updated i3_startup.sh

--- .i3_startup.sh
+++ .i3_startup.sh
@@ -4,6 +4,10 @@
 # Wait a while
 sleep 1s;
 
+# Check if external screen is attached and if so switch laptop one off
+xrandr -q | grep connected | grep 'HDMI-1' && xrandr --output HDMI-1 --mode \
+    1920x1080 --rate 60.00 --output eDP-1 --off
+
 # Set gb keyboard
 setxkbmap gb
 
@@ -14,11 +18,12 @@ xscreensaver -nosplash &
 nitrogen --restore &
 
 # Enable touchpad tapping and typing mod
-# (synclient TapButton1=1 TapButton2=2 TapButton3=3; syndaemon -t -k -i 2 -d) &
+# Two finger right click
+(synclient TapButton1=1 TapButton2=3 TapButton3=3; syndaemon -t -k -i 2 -d) &
 
 # Reverse scrolling
-# synclient VertScrollDelta=-111
-# synclient HorizScrollDelta=-111
+synclient VertScrollDelta=-111
+synclient HorizScrollDelta=-111
 
 # Start an lxterminal
 lxterminal &
@@ -27,14 +32,18 @@ lxterminal &
 (killall redshift-gtk; redshift-gtk -l 51.61:-0.32) &
 
 # Transmission client
-transmission-qt &
+# transmission-qt &
+# transgui &
+
+# Clipboard info
+/home/nasef/github/ClipboardInfo/clipboard_info.py &
 
 # Keepassx
-keepassx &
+# keepassx &
 
 # Dropbox
-(sleep 10s && ~/.dropbox-dist/dropboxd) &
+# (sleep 10s && ~/.dropbox-dist/dropboxd) &
 
 # Python power management script
 # python ~/code/scripts/power_man.py &
-#python ~/code/scripts/power_man.py >> ~/code/scripts/power_man.log 2>&1  & # debugging
+# python ~/code/scripts/power_man.py >> ~/code/scripts/power_man.log 2>&1  & # debugging
